0f61d8e7741dde7dd3e2e21555286a7ec92ae149
Nabeel <nabeeltahirdeveloper@gmail.com>
2024-05-09 00:03:15 +0500

    chore: Add initial server setup with Express, cron, and fetch dependencies

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cron from 'node-cron';
+import fetch from 'node-fetch';
+
+const app = express();
+const port = 3001; // Customize as needed
+
+// Simple Hello World route
+app.get('/', (req, res) => {
+  res.send('This is nodejs server for funds reimbursement!');
+});
+
+// Schedule a job to run on the 1st of every month at 00:00 (midnight)
+cron.schedule('* * * * *', async () => {
+
+  console.log('Running scheduled task: calling external API on the 1st of the month.');
+
+  try {
+    const response = await fetch('http://localhost:3000/api/leaves/leavesUpdate', { method: 'POST' });
+    const data = await response.json();
+    console.log('API Response:', data.message);
+  } catch (error) {
+    console.error('Error calling the API:', error);
+  }
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
